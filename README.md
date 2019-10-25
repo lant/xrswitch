05541a75c5449d775d7df1d88e50551fd57f7be7
Marc de Palol <marc.depalol@adevinta.com>
2019-10-25 16:22:16 +0200

    Configuration is not hardcoded anymore.

--- README.md
+++ README.md
@@ -3,4 +3,9 @@
 
 This is a very stupid, simple and naive tool to let me select the display profile for my box. 
 
-While learning `go` in the process. 
+## Usage
+You just need a configurtion file in your XDG_CONFIG_HOME or in `.home/xrswitch`
+
+There you'll list the profiles and the `xrandr` command needed to tune the screens. 
+
+There is an `toml` configuration example provided in the code. 
--- main.go
+++ main.go
@@ -2,57 +2,96 @@ package main
 
 import (
 	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/mitchellh/go-homedir"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
+type tomlConfig struct {
+	Profiles map[string]profile
+}
+
+type profile struct {
+	Name    string
+	Command string
+}
+
+var configDirName = "xrswitch"
 
 func main() {
-	commands := map[int][]string {
-		1: { "--output",  "eDP-1-1", "--mode", "1600x900", "--output", "DP-1-1", "--off", "--output", "DP-1-3", "--off" },
-		2: { "--output",  "eDP-1-1", "--off", "--output", "DP-1-1", "--mode", "2560x1440" },
-		3: { "--output",  "eDP-1-1", "--off", "--output", "DP-1-3", "--mode", "2560x1440", "--primary" },
-		4: { "--output",  "eDP-1-1", "--off", "--output", "DP-1-3", "--mode", "1920x1080" },
+
+	var config tomlConfig
+	var configDir, error = GetDefaultConfigDir()
+
+	if error != nil {
+		fmt.Print(error)
+		os.Exit(-1)
+	}
+
+	var configFile = filepath.Join(configDir, "xrswitch.toml")
+	if _, err := toml.DecodeFile(configFile, &config); err != nil {
+		fmt.Println(err)
+		return
 	}
 
 	fmt.Println("Select display setup:")
-	fmt.Println("1) Laptop")
-	fmt.Println("2) Office")
-	fmt.Println("3) Home")
-	fmt.Println("4) Parents")
+	for profile := range config.Profiles {
+		fmt.Println(profile)
+	}
 	fmt.Println("---------------------------")
 
-  var optionsSize = len(commands)
-
-	var selected int
+	var selected string
 	fmt.Print("Select Option > ")
-	_, inputError := fmt.Scanf("%d", &selected)
+	_, inputError := fmt.Scanf("%s", &selected)
 	if inputError != nil {
 		fmt.Print(inputError)
 		os.Exit(-1)
 	}
 
-  if selected > optionsSize || selected <= 0 {
+	if _, ok := config.Profiles[selected]; !ok {
 		fmt.Println("Invalid option")
 		os.Exit(-1)
-  }
+	}
 
-  execRandr(commands[selected])
-  execWallpaper()
+	execRandr(config.Profiles[selected].Command)
+	execWallpaper()
 }
 
-func execRandr(randr []string) {
-	command := exec.Command("xrandr", randr...)
+func GetDefaultConfigDir() (string, error) {
+	var configDirLocation string
+
+	homeDir, err := homedir.Dir()
+	if err != nil {
+		return "", err
+	}
+
+	// Use the XDG_CONFIG_HOME variable if it is set, otherwise
+	// $HOME/.config/example
+	xdgConfigHome := os.Getenv("XDG_CONFIG_HOME")
+	if xdgConfigHome != "" {
+		configDirLocation = xdgConfigHome
+	} else {
+		configDirLocation = filepath.Join(homeDir, ".config", configDirName)
+	}
+
+	return configDirLocation, nil
+}
+
+func execRandr(randr string) {
+	command := exec.Command("xrandr", strings.Fields(randr)...)
 	err := command.Run()
 	if err != nil {
-		fmt.Printf("xrandr failed with: %v",err)
+		fmt.Printf("xrandr failed with: %v", err)
 	}
 }
 
 func execWallpaper() {
-  command := exec.Command("nitrogen", "--set-scaled", "/home/marc/wallpapers/amps.jpg")
+	command := exec.Command("nitrogen", "--set-scaled", "/home/marc/wallpapers/amps.jpg")
 	err := command.Run()
 	if err != nil {
-		fmt.Printf("Could not set the wallpaper: %v",err)
+		fmt.Printf("Could not set the wallpaper: %v", err)
 	}
 }
